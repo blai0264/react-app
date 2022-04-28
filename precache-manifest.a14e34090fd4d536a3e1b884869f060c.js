d72fdd55a68a0f21c03d823ef36c5a534b105d0a
Marcus <mblais93@gmail.com>
2022-04-28 14:36:28 -0400

    Updates

---
+++ precache-manifest.a14e34090fd4d536a3e1b884869f060c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "933ab45ff25a3079801b",
+    "url": "/react-app/static/css/main.f85ab729.chunk.css"
+  },
+  {
+    "revision": "933ab45ff25a3079801b",
+    "url": "/react-app/static/js/main.933ab45f.chunk.js"
+  },
+  {
+    "revision": "6a8817d298c73ae6155b",
+    "url": "/react-app/static/js/1.6a8817d2.chunk.js"
+  },
+  {
+    "revision": "ec46125784e0b1f6cca6",
+    "url": "/react-app/static/js/runtime~main.ec461257.js"
+  },
+  {
+    "revision": "f2a54b0b21531377d973d2f5c05591cf",
+    "url": "/react-app/static/media/Loading.f2a54b0b.svg"
+  },
+  {
+    "revision": "ee6429951266758baf69c40e6273a788",
+    "url": "/react-app/index.html"
+  }
+];
